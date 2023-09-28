a5d42cd42bf5e193c42a2eb385ddcb9d6ae5fc42
Rahul Patel <rahulpatel43433@gmail.com>
2023-09-28 19:46:40 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -19,7 +19,14 @@ connectDB();
 const app = express();
 
 //middelwares
-app.use(cors());
+// app.use(cors());
+app.use(cors(
+  {
+      origin: ["https://deploy-mern-frontend.vercel.app"],
+      methods: ["POST", "GET"],
+      credentials: true
+  }
+));
 app.use(express.json());
 app.use(morgan("dev"));
 
