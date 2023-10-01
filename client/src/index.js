795f3d40c85f486387d4408c416440783bdd91d3
Rahul Patel <rahulpatel43433@gmail.com>
2023-10-01 20:57:48 +0530

    backend url added

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -12,7 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 // import './styles.css'
-
+axios.defaults.baseURL = 'https://backend-glia.onrender.com';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthProvider>
